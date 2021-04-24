a1896862006c516bc71f8247b0a8f5d6a1319c30
terabytesoftw <terabytesoftw@gmail.com>
2021-04-24 14:52:46 -0400

    Fix config translator.

--- src/ViewInjection/ParametersViewInjection.php
+++ src/ViewInjection/ParametersViewInjection.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Simple\View\Bulma\ViewInjection;
 
+use Yiisoft\Aliases\Aliases;
 use Yiisoft\Assets\AssetManager;
+use Yiisoft\Factory\Definition\Reference;
 use Yiisoft\Router\UrlGeneratorInterface;
 use Yiisoft\Router\UrlMatcherInterface;
+use Yiisoft\Translator\CategorySource;
+use Yiisoft\Translator\Formatter\Intl\IntlMessageFormatter;
+use Yiisoft\Translator\Message\Php\MessageSource;
 use Yiisoft\Translator\TranslatorInterface;
 use Yiisoft\Yii\View\ContentParametersInjectionInterface;
 use Yiisoft\Yii\View\LayoutParametersInjectionInterface;
@@ -19,15 +24,19 @@ final class ParametersViewInjection implements ContentParametersInjectionInterfa
     private UrlMatcherInterface $urlMatcher;
 
     public function __construct(
+        Aliases $aliases,
         AssetManager $assetManager,
         TranslatorInterface $translator,
         UrlGeneratorInterface $urlGenerator,
         UrlMatcherInterface $urlMatcher
     ) {
+        $this->aliases = $aliases;
         $this->assetManager = $assetManager;
         $this->translator = $translator;
         $this->urlGenerator = $urlGenerator;
         $this->urlMatcher = $urlMatcher;
+
+        $this->createCategorySource();
     }
 
     public function getContentParameters(): array
@@ -48,5 +57,14 @@ final class ParametersViewInjection implements ContentParametersInjectionInterfa
             'urlGenerator' => $this->urlGenerator,
             'urlMatcher' => $this->urlMatcher,
         ];
+    }
+
+    private function createCategorySource(): void 
+    {
+        $messageReader = new MessageSource($this->aliases->get('@simple-view-bulma/storage/translation'));
+        $messageFormatter = new IntlMessageFormatter();
+        $categorySource = new CategorySource('simple-view-bulma', $messageReader, $messageFormatter);
+
+        $this->translator->addCategorySources([$categorySource]);
     }
 }
